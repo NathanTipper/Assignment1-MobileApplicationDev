8ca41d32c708080756a31ab97af8170d3023a7e8
W1llbedone <n.tipper05@gmail.com>
2019-02-14 04:29:46 -0700

    Done...

--- www/js/ABetterMe.js
+++ www/js/ABetterMe.js
@@ -43,7 +43,7 @@ setTimeout(function() {
 }, 3000)
 
 // Constant Variables
-const habitPopup = app.popup.create({
+const addHabitPopup = app.popup.create({
   content: `
   <div class="popup popup-add-habit">
     <h1>Add Habit</h1>
@@ -58,7 +58,7 @@ const habitPopup = app.popup.create({
             </div>
           </div>
         </li>
-        <li class="radio-option">
+        <li id="daily-radio" class="radio-option">
           <label class="item-radio item-content">
             <input type="radio" name="frequency" value="daily"/>
             <i class="icon icon-radio"></i>
@@ -67,7 +67,7 @@ const habitPopup = app.popup.create({
             </div>
           </label>
         </li>
-        <li class="radio-option">
+        <li id="monthly-radio" class="radio-option">
           <label class="item-radio item-content">
             <input type="radio" name="frequency" value="monthly"/>
             <i class="icon icon-radio"></i>
@@ -76,7 +76,7 @@ const habitPopup = app.popup.create({
             </div>
           </label>
         </li>
-        <li class="radio-option">
+        <li id="custom-radio" class="radio-option">
           <label class="item-radio item-content">
             <input type="radio" name="frequency" value="custom"/>
             <i class="icon icon-radio"></i>
@@ -89,7 +89,7 @@ const habitPopup = app.popup.create({
           <div class="item-inner">
             <div class="item-title item-label">Day of Month</div>
             <div class="item-input-wrap">
-              <input type="number" name="day-of-month" min="1" max="31" placeholder="Day of the month to repeat habit">
+              <input type="number" name="day-of-month" min="1" max="31" placeholder="Day to repeat habit">
               <span class="input-clear-button"></span>
             </div>
           </div>
@@ -108,7 +108,7 @@ const habitPopup = app.popup.create({
             </p>
           </div>
         </li>
-        <li id="popup-time-reminder-input" >
+        <li id="popup-time-reminder-input">
           <div class="item-inner no-hairlines">
             <div class="item-title item-label">Reminder Time</div>
             <div>
@@ -116,13 +116,27 @@ const habitPopup = app.popup.create({
             </div>
           </div>
         </li>
+        <li id="popup-add-journal">
+          <label class="item-checkbox item-content">
+            <input type="checkbox"/>
+            <i class="icon icon-checkbox"></i>
+            <div class="item-inner">
+              <div class="item-title">Add Journal?</div>
+            </div>
+          </label>
+        </li>
       </ul>
     </div>
-    <p class="align-center">
-      <button class="small-button button button-fill" onclick="AddHabit()">Create</button>
-    </p>
+    <div id="edit-habit-buttons" class="block">
+      <div class="row">
+        <button id="finished-editing-button" class="col button button-fill">Done</button>
+        <button id="delete-habit-button" class="col button button-fill">Delete</button>
+      </div>
+    </div>
+    <button id="create-habit-button" class="small-button button button-fill" onclick="AddHabit()">Create</button>
   </div>`
 });
+
 
 // Global Variables
 var objectiveNumber = 6;
@@ -161,7 +175,7 @@ $$(document).on('page:init', '.page[data-name="main"]', function(e) {
   UpdateHabits()
 
   $("#add-habit-button").click(function() {
-    habitPopup.open();
+    addHabitPopup.open();
     $(".radio-option input").on("change", function() {
       switch($(".radio-option input:checked").val()) {
         case 'daily':
@@ -184,11 +198,26 @@ $$(document).on('page:init', '.page[data-name="main"]', function(e) {
 });
 
 // Classes
-function Habit(name, reminders, journalEntry) {
+function Habit(name, info, journalEntry) {
   this.name = name;
-  this.reminders = reminders;
-  this.journalEntry = journalEntry;
+  this.info = info;
+  this.journals = [journalEntry];
   this.streak = 0;
+}
+
+function HabitInfo(frequency, dayOfMonth, timeOfDay, days) {
+  this.frequency = frequency;
+  if(this.frequency === 'monthly')
+    this.dayOfMonth = dayOfMonth;
+  else
+    this.dayOfMonth = 0;
+
+  this.timeOfDay = timeOfDay;
+
+  if(this.frequency === 'custom')
+    this.days = days;
+  else
+    this.days = [];
 }
 
 // Functions
@@ -217,21 +246,118 @@ function AddHabit() {
       break;
   }
 
-  let reminders = { frequency: frequency, timeOfDay: timeOfDay, dayOfMonth: dayOfMonth, days: days };
-  // fake data for now until we get journals working
+  let info = new HabitInfo(frequency, dayOfMonth, timeOfDay, days);
+  console.log(info);
   let journalEntry = "I am being run down by a hippopatimus... Please help.";
-  ResetHabitPopup();
-  habitPopup.close();
-  CreateHabit(nameOfHabit, reminders, journalEntry);
+  ResetAddHabitPopup();
+  addHabitPopup.close();
+  CreateHabit(nameOfHabit, info, journalEntry);
 }
 
 function UpdateHabits() {
   let habitsList = $("#habits-list");
-  for(let i = 0; i < habits.length; ++i) {
-    habitsList.append(`<li><a onclick='editHabit(${1})' class='item-link item-content'><div class='item-inner'>\
-                      <div class='item-title'>${habits[i].name}</div>\
-                      <div class='item-after'>${habits[i].streak}</div></div></a></li>`);
+  let habitsListLength = $("#habits-list li").length;
+
+  if(habitsListLength < habits.length) {
+    for(let i = habitsListLength; i < habits.length; ++i) {
+      habitsList.append(`<li><a onclick='editHabit(${i})' class='item-link item-content'><div class='item-inner'>\
+                        <div class='item-title'>${habits[i].name}</div>\
+                        <div class='item-after'>${habits[i].streak}</div></div></a></li>`);
+    }
   }
+  else {
+    habitsList.empty();
+    for(let i = 0; i < habits.length; ++i) {
+      habitsList.append(`<li><a onclick='editHabit(${i})' class='item-link item-content'><div class='item-inner'>\
+                        <div class='item-title'>${habits[i].name}</div>\
+                        <div class='item-after'>${habits[i].streak}</div></div></a></li>`);
+    }
+  }
+  if($("#habits-list li").length > 0) {
+    $("#habits-list").show();
+    $("#no-habits-message").hide();
+  }
+  else {
+    $("#habits-list").hide();
+    $("#no-habits-message").show();
+  }
+}
+
+function editHabit(index) {
+  addHabitPopup.open();
+  let habitToEdit = habits[index];
+  let habitInfo = habits[index].info;
+  $("#habit-name input").val(habitToEdit.name);
+  $("#popup-time-reminder-input input").val(habitInfo.timeOfDay);
+  switch (habitInfo.frequency) {
+    case 'daily':
+      $("#daily-radio input").prop('checked', true);
+      break;
+
+    case 'monthly':
+      $("#monthly-radio input").prop("checked", true);
+      $("#pop-custom-day-input input").val(habitInfo.dayOfMonth);
+      break;
+    case 'custom':
+      $("#custom-radio input").prop("checked", true);
+      let days = $("#popup-custom-day-input input");
+      if(habitInfo.days.length > 0) {
+        for(let i = 0, k = 0; i < days.length; ++i) {
+          if(days[i].val() == habitInfo.days[k]) {
+            days[i].prop("checked", true);
+            ++k;
+            if(k >= habitInfo.days.length)
+              break;
+          }
+        }
+      }
+  }
+
+  $("#edit-habit-buttons").show();
+  $("#create-habit-button").hide();
+
+  $("#finished-editing-button").click(function() {
+    DoneEditingHabit(index);
+  })
+
+  $("#delete-habit-button").click(function() {
+    DeleteHabit()
+  })
+}
+
+function DoneEditingHabit(index) {
+  let nameOfHabit = $("#habit-name input").val();
+  let frequency = $(".radio-option input:checked").val();
+  let timeOfDay = $("#popup-time-reminder-input input").val();
+
+  let dayOfMonth = 0;
+  let days = [];
+  switch(frequency) {
+    case 'monthly':
+      dayOfMonth = $("#popup-day-input input").val();
+      break;
+    case 'custom':
+      let checks = $("#popup-custom-day-input input:checked");
+      for(let i = 0; i < checks.length; ++i) {
+        days.push(checks[i].value);
+      }
+      break;
+  }
+
+  let info = new HabitInfo(frequency, dayOfMonth, timeOfDay, days);
+  let journalEntry = "I am being run down by a hippopatimus... Please help.";
+  ResetAddHabitPopup();
+  addHabitPopup.close();
+  habits[index].name = nameOfHabit;
+  habits[index].info = info;
+  UpdateHabits(index);
+}
+
+function DeleteHabit(index) {
+  habits.splice(index, 1);
+  UpdateHabits();
+  ResetAddHabitPopup();
+  addHabitPopup.close();
 }
 
 // Obtain all the objectives the user types in and put them in a list.
@@ -265,7 +391,7 @@ function GetWhy() {
   mainView.router.navigate('/main/');
 }
 
-function ResetHabitPopup() {
+function ResetAddHabitPopup() {
   $("#habit-name input").val("");
   $(".radio-option input:checked").prop('checked', false);
   $("#popup-time-reminder-input input").val("");
@@ -275,6 +401,4 @@ function ResetHabitPopup() {
   for(let i = 0; i < checks.length; ++i) {
     checks[i].prop('checked', false);
   }
-
-
 }
